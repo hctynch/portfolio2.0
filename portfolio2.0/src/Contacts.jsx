4958c62f3ab7b40c7ed7a2bb126d16af2b52062b
Hunt Tynch <tynchhunt@gmail.com>
2025-01-30 06:26:23 -0500

    Display looking better on mobile now.

--- portfolio2.0/src/Contacts.jsx
+++ portfolio2.0/src/Contacts.jsx
@@ -2,7 +2,7 @@ import { AiFillInstagram, AiFillLinkedin, AiFillMail } from 'react-icons/ai'
 
 function Contacts() {
   return (
-    <div className="z-50 fixed top-100 left-6 sm:top-255 sm:left-8 flex flex-col h-screen opacity-70">
+    <div className="z-50 fixed bottom-2 left-6 sm:left-8 flex flex-col opacity-70">
       <a href="https://linkedin.com/in/hunt-tynch" className="hover:scale-120  transform transition-transform duration-300">
         <AiFillLinkedin className="my-2 w-10 h-10 md:w-15 md:h-15 text-white hover:text-blue-500"/>
       </a>
--- portfolio2.0/src/Navbar.jsx
+++ portfolio2.0/src/Navbar.jsx
@@ -8,26 +8,33 @@ function Navbar() {
 
   return (
     <div className="fixed z-50 flex h-16 w-full top-6 items-center justify-center md:h-20">
-      <div className="m-auto flex h-16 lg:w-full w-lg items-center justify-between shadow-white/5 shadow-lg lg:top-6  lg:h-20 lg:max-w-screen-lg lg:mx-auto bg-white/5 backdrop-blur-lg rounded-full">
-        <a href="#home">
-          <p className="ml-5 sm:ml-8 mr-auto text-3xl sm:text-5xl font-[Source_Code_Pro] font-medium text-white">
+      <div className="m-auto flex h-16 md:w-full w-lg items-center justify-between shadow-white/5 shadow-lg md:top-6  md:h-20 md:max-w-screen-lg md:mx-auto bg-white/5 backdrop-blur-lg rounded-full">
+        <a href="#home" className="z-50">
+          <p className="ml-5 md:ml-8 mr-auto text-3xl md:text-5xl font-[Source_Code_Pro] font-medium text-white hover:underline">
             HT
           </p>
         </a>
-        <div className="absolute top-6 left-1/5 sm:left-1/3 z-10 flex h-screen w-full">
+        <div className="mx-auto absolute top-6 justify-end md:justify-center z-10 flex h-screen w-full ">
         {navs.map((item, index) =>
-          <a key={index} href={item.href} className="mx-5 sm:mx-8">
-            <div className="text-sm sm:text-lg text-white font-[Source_Code_Pro] font-bold">
+          <a key={index} href={item.href} className="mx-5 md:mx-8">
+            <div className="text-sm md:text-lg text-white font-[Source_Code_Pro] font-bold hover:underline">
               {item.title}
             </div>
           </a>
         )}
         </div>
         <a key="resume" 
-          className="z-50 cursor-pointer absolute top-3 right-4 sm:top-5 sm:right-12" 
+          className="hidden md:flex z-50 cursor-pointer absolute right-4 md:top-5 md:right-12" 
           href="/Hunt_Tynch_Resume.pdf" 
           download>
-          <div className="rounded-full text-center items-center flex justify-center w-24 h-10 bg-gradient-to-b from-cyan-500 to-purple-500 text-white font-[Source_Code_Pro] font-medium">
+          <div className="rounded-2xl text-center items-center flex justify-center w-18 h-10 bg-gradient-to-b from-cyan-500 to-purple-500 text-white font-[Source_Code_Pro] font-medium">
+            Resume
+          </div>
+        </a>
+        <a className="md:hidden z-50"
+          href="/Hunt_Tynch_Resume.pdf" 
+          download>
+            <div className="fixed top-22 left-2 rounded-2xl text-center items-center flex justify-center w-18 h-10 bg-gradient-to-b from-cyan-500 to-purple-500 text-white font-[Source_Code_Pro] font-medium">
             Resume
           </div>
         </a>
